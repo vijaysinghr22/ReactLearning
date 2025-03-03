25ea0ca6b87bebd0aa64181119b6273c1ecf3221
vijaysinghr22 <30354218+vijaysinghr22@users.noreply.github.com>
2025-03-03 09:52:38 +0530

    Update ProductList.jsx

--- redux/ProductList.jsx
+++ redux/ProductList.jsx
@@ -3,15 +3,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { setProducts, deleteProduct } from "./productSlice";
 import productService from "./productService";
 import ProductForm from "./ProductForm";
+import ConfirmationPopup from "./ConfirmationPopup";
 
 const ProductList = () => {
   const dispatch = useDispatch();
   const products = useSelector(state => state.products.list);
   const [modalData, setModalData] = useState({ open: false, product: null });
+  const [confirmMessage, setConfirmMessage] = useState("");
 
   useEffect(() => {
     productService.fetchProducts().then(data => dispatch(setProducts(data)));
   }, [dispatch]);
+
+  const handleDelete = (id) => {
+    dispatch(deleteProduct(id));
+    setConfirmMessage("Product deleted successfully!");
+  };
 
   return (
     <div>
@@ -22,14 +29,20 @@ const ProductList = () => {
           <li key={p.id}>
             {p.name}
             <button onClick={() => setModalData({ open: true, product: p })}>Edit</button>
-            <button onClick={() => dispatch(deleteProduct(p.id))}>Delete</button>
+            <button onClick={() => handleDelete(p.id)}>Delete</button>
           </li>
         ))}
       </ul>
 
       {modalData.open && (
-        <ProductForm data={modalData} close={() => setModalData({ open: false, product: null })} />
+        <ProductForm
+          data={modalData}
+          close={() => setModalData({ open: false, product: null })}
+          setConfirmMessage={setConfirmMessage}
+        />
       )}
+
+      {confirmMessage && <ConfirmationPopup message={confirmMessage} close={() => setConfirmMessage("")} />}
     </div>
   );
 };
